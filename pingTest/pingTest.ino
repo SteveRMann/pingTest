cd32101dc348b83cedd16cc57d508e28b50670b9
Stephen Mann <steve@mann.ws>
2023-02-21 01:09:13 -0500

    Removed two tabs

--- pingTest/pingTest.ino
+++ pingTest/pingTest.ino
@@ -1,6 +1,6 @@
 #define SKETCH "pingTest"
 #define VERSION "1.00"           // Four characters
-#define hostPrefix "PINGER"      // Six characters max
+
 /*
    This program is a test of the ping function
 */
@@ -13,27 +13,83 @@
 
 // setup_wifi vars
 char macBuffer[24];       // Holds the last three digits of the MAC, in hex.
-char hostNamePrefix[] = hostPrefix;
-char hostName[24];        // Holds hostNamePrefix + the last three bytes of the MAC address.
+char pingIP[20];          // IP that we're going to ping.
+
+//****************************** SERIAL **************************
+void beginSerial() {
+  Serial.begin(115200);
+  delay(100);
+  Serial.println();
+  Serial.println();
+  Serial.println(SKETCH);
+  Serial.println(VERSION);
+  Serial.println();
+}
 
 
-//************************************** SETUP *******************************
+//****************************** WiFi **************************
+void setup_wifi() {
+#ifndef Kaywinnet
+#include <Kaywinnet.h>
+#endif
+  
+  Serial.println();
+  Serial.println("Connecting to ");
+  Serial.println(MY_SSID);
+
+  WiFi.mode(WIFI_STA);
+  //WiFi.enableInsecureWEP();
+  WiFi.hostname(SKETCH);
+  WiFi.begin(MY_SSID, MY_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(WiFi.status());
+    Serial.print(" ");
+  }
+
+  Serial.println("\nWiFi connected, ");
+  Serial.print("MAC Address: ");
+  Serial.println(WiFi.macAddress());
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("Hostname: ");
+  Serial.println(WiFi.hostname().c_str());
+
+}
+
+
+//****************************** PING **************************
+void pingit() {
+  Serial.print(pingIP);
+  Serial.print(F(": "));
+  if (Ping.ping(pingIP)) {                    // Ping
+    Serial.println("OK");
+  } else {
+    Serial.println("Fail");
+  }
+  yield();                                    // Reset the WDT on the ESP8266
+}
+
+//****************************** SETUP **************************
 void setup() {
   beginSerial();
   setup_wifi();
 }
 
 
-//************************************** LOOP *******************************
+//****************************** LOOP **************************
 void loop() {
-  char pingIP[20];                            // IP that we're going to ping.
   strcpy(pingIP, "192.168.1.1");
-  yield();                                    // Reset the WDT on the ESP8266
+  pingit();
+  strcpy(pingIP, "192.168.1.57");
+  pingit();
+  strcpy(pingIP, "192.168.1.124");
+  pingit();
+  strcpy(pingIP, "192.168.1.198");
+  pingit();
+  strcpy(pingIP, "127.0.0.1");
+  pingit();
 
-  if (Ping.ping(pingIP)) {                    // Ping
-    Serial.println("OK");
-  } else {
-    Serial.println("Fail");
-  }
+  Serial.println(F("---------------"));
   delay(1000);
 }
--- pingTest/serial.ino
+++
@@ -1,9 +0,0 @@
-void beginSerial() {
-  Serial.begin(115200);
-  delay(10);
-  Serial.println("");
-  Serial.println("");
-  Serial.println(SKETCH);
-  Serial.println(VERSION);
-  Serial.println("");
-}
